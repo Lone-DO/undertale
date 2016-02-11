8b708f6a7036fd8ec3d4d15eee95cad89b1afc39
lone-DO <Lone-DO@users.noreply.github.com>
2016-02-11 16:57:18 -0500

    Updated logs and HTML's

---
+++ UnderTale/js/java.js
@@ -0,0 +1,37 @@
+//Index Flowey Script
+
+function floweySet(type) {
+	var sprite = document.getElementById('flowey');
+	switch(type) {
+			
+		case 'Windows': 
+			sprite.style["background"] = 'transparent url(images/floweywinkBIG.png) no-repeat';
+			break;
+			
+		case 'Mac': 
+			sprite.style["background"] = 'transparent url(images/floweyhappyBIG.png) no-repeat';
+			break;
+			
+		case 'Twitter': 
+			sprite.style["background"] = 'transparent url(images/floweycoolBIG.png) no-repeat';
+			break;
+			
+		case 'Tumblr': 
+			sprite.style["background"] = 'transparent url(images/floweycuteBIG.png) no-repeat';
+			break;
+			
+		case 'Store': 
+			sprite.style["background"] = 'transparent url(images/floweymoneyeyesBIG.png) no-repeat';
+			break;
+			
+		case 'KS': 
+			sprite.style["background"] = 'transparent url(images/floweymoneyeyesBIG2.png) no-repeat';
+			break;
+			
+		default:
+			sprite.style["background"] = 'transparent url(images/floweyBIG.png) no-repeat';
+			break;
+	}
+}
+
+//

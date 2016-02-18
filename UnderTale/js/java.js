219be7847edf1142b76cc9f2959652e2c2f9e869
lone-DO <Lone-DO@users.noreply.github.com>
2016-02-18 09:04:40 -0500

    Batch update
    Added Fonts to be used for Fleshed out text, Fixed 404 errors with image
    addresses, linked jquery in index (not in use yet),

--- UnderTale/js/java.js
+++ UnderTale/js/java.js
@@ -5,33 +5,31 @@ function floweySet(type) {
 	switch(type) {
 			
 		case 'Windows': 
-			sprite.style["background"] = 'transparent url(images/floweywinkBIG.png) no-repeat';
+			sprite.style["background"] = 'transparent url(../images/floweywinkBIG.png) no-repeat';
 			break;
 			
 		case 'Mac': 
-			sprite.style["background"] = 'transparent url(images/floweyhappyBIG.png) no-repeat';
+			sprite.style["background"] = 'transparent url(../images/floweyhappyBIG.png) no-repeat';
 			break;
 			
 		case 'Twitter': 
-			sprite.style["background"] = 'transparent url(images/floweycoolBIG.png) no-repeat';
+			sprite.style["background"] = 'transparent url(../images/floweycoolBIG.png) no-repeat';
 			break;
 			
 		case 'Tumblr': 
-			sprite.style["background"] = 'transparent url(images/floweycuteBIG.png) no-repeat';
+			sprite.style["background"] = 'transparent url(../images/floweycuteBIG.png) no-repeat';
 			break;
 			
 		case 'Store': 
-			sprite.style["background"] = 'transparent url(images/floweymoneyeyesBIG.png) no-repeat';
+			sprite.style["background"] = 'transparent url(../images/floweymoneyeyesBIG.png) no-repeat';
 			break;
 			
 		case 'KS': 
-			sprite.style["background"] = 'transparent url(images/floweymoneyeyesBIG2.png) no-repeat';
+			sprite.style["background"] = 'transparent url(../images/floweymoneyeyesBIG2.png) no-repeat';
 			break;
 			
 		default:
-			sprite.style["background"] = 'transparent url(images/floweyBIG.png) no-repeat';
+			sprite.style["background"] = 'transparent url(../images/floweyBIG.png) no-repeat';
 			break;
 	}
 }
-
-//

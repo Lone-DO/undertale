2341bc845754a5c2a114328fb641771fe4807cc7
lone-DO <Lone-DO@users.noreply.github.com>
2016-02-25 15:24:05 -0500

    Add Preview UI pieces
    Ajax.load features, Css still needs work on, removed useless audio files
    for now till implemented later.

--- UnderTale/js/jquery.js
+++ UnderTale/js/jquery.js
@@ -1,5 +1,39 @@
+////To be worked on, Animation Fade in and out
+//$(document).ready(function()
+//{
+//    $('#navigation').on('click','a',function()
+//    {
+//        // fade out all open subcontents
+//        $('.subcontent:visible').fadeOut();
+//        // fade in new selected subcontent
+//        $('.subcontent[id='+$(this).attr('id')+']').fadeIn();
+//    });
+//});
 
-
-$("#embed").load("FAQ.html", function () {
-    alert("Load was performed.");
+$(function () {
+  'use strict';
+  $("#nav_about").on("click", 'a', function () {
+    $("#main").load("about.html");
+    alert('loaded');
+  });
+    
+  $("#nav_demo").on("click", 'a', function () {
+    $("#main").load("demo.html");
+    alert('loaded');
+  });
+    
+  $("#nav_contact").on("click", 'a', function () {
+    $("#main").load("contact.html");
+    alert('loaded');
+  });
+  
+  $("#nav_faq").on("click", 'a', function () {
+    $("#main").load("faq.html");
+    alert('loaded');
+  });
+  
+  $("#nav_troubleshoot").on("click", 'a', function () {
+    $("#main").load("troubleshoot.html");
+    alert('loaded');
+  });
 });

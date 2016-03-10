cb6d5195fa9c97c38aeceb3df350402a31dc765b
lone-DO <Lone-DO@users.noreply.github.com>
2016-03-10 17:30:36 -0500

    Reinserted Merch page For Later Beta

--- Ember.app/controllers/new-mail.js
+++ Ember.app/controllers/new-mail.js
@@ -3,12 +3,28 @@ Undertale.NewMailController = Ember.Controller.extend({
     //Create mail
     save: function () {
       'use strict';
-      var newMail = this.store.createRecord('mail', {
-        //attaches to {{user}}
-        user: this.get('user'),
-        //attaches to {{text}}
-        text: this.get('text')
-      });
+      var $userReady = this.user.val().length,
+        $textReady = this.text.val().length,
+        newMail = this.store.createRecord('mail', {
+          //attaches to {{user}}
+          user: this.get('user'),
+          //attaches to {{text}}
+          text: this.get('text')
+        });
+//      function onchange() {
+//        if ($userReady > 3) {
+//          console.log("active");
+//        } else {
+//          console.log("disabled");
+//        }
+//      }
+      //Disable Save Button
+        //Check if User Field is Valid
+          //Valid if user.val().length > 3
+        //Check if Text Field is Valid
+          //Valid if text.val().length > 5
+      //If Valid = True.then(Enable Save Button)
+      
       //Save Mail
       newMail.save();
       this.transitionToRoute('mailbox');

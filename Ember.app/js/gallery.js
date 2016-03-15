9157bd65ab5d3392ceb5242b0bd8efa22d36484c
lone-DO <Lone-DO@users.noreply.github.com>
2016-03-15 13:35:20 -0400

    Tumblr Post...

--- Ember.app/js/gallery.js
+++ Ember.app/js/gallery.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   'use strict';
+  
   //FLICKR API CALLS
   $('#showFlickr').click(function () {
     // AJAX
@@ -27,7 +28,8 @@ $(document).ready(function () {
     // Authenticate via API Key
     var tumblrAPI = "http://api.tumblr.com/v2/blog/undertale.tumblr.com/info?api_key=vloc4eXTLszkJIWjma1zAvx9wcYnXTaGS1QdKiTGHR3epAclxP",
       tumblrAvt = "http://api.tumblr.com/v2/blog/undertale.tumblr.com/avatar/128?api_key=vloc4eXTLszkJIWjma1zAvx9wcYnXTaGS1QdKiTGHR3epAclxP",
-      tumblrPost = "http://api.tumblr.com/v2/blog/undertale.tumblr.com/posts?api_key=vloc4eXTLszkJIWjma1zAvx9wcYnXTaGS1QdKiTGHR3epAclxP",
+      tumblrPost = "http://api.tumblr.com/v2/blog/undertale.tumblr.com/posts/text?api_key=vloc4eXTLszkJIWjma1zAvx9wcYnXTaGS1QdKiTGHR3epAclxP",
+      tumblrPhoto = "http://api.tumblr.com/v2/blog/undertale.tumblr.com/posts/photo?api_key=vloc4eXTLszkJIWjma1zAvx9wcYnXTaGS1QdKiTGHR3epAclxP",
       text = '';
     //T-Header
     $.ajax({
@@ -39,27 +41,58 @@ $(document).ready(function () {
     });
     
     //T-Avatar
+//    $.ajax({
+//      url: tumblrAvt,
+//      dataType: 'jsonp',
+//      success: function (avatar) {
+//        console.log('Image');
+//        $("#myDivId").attr('src', avatar.response.avatar_url);
+//      }
+//    });
+    
+    //T-Photos
+//    var opts = {
+//      format: 'json'
+//    };
+//    function displayPhotos(data) {
+//      var photoHTML = '<ul>';
+//      $.each(data.response.posts, function (i, photo) {
+//        photoHTML += '<li>' + '<a href="' + photo.image_permalink + '" class="image">';
+//        photoHTML += '<img src="' + photo.image_permalink + '"></a></li>';
+//      });
+//      photoHTML += '</ul>';
+//    }
+//    $.getJSON(tumblrPhoto, opts, displayPhotos);
+    
+//    $.getJSON(tumblrPhoto, {
+//      tags: 'undertale',
+//      format: 'JSONP'
+//    },
+//      function (data) {
+//        var photoHTML = '<ul>';
+//        $.each(data.posts.photos, function (i, photo) {
+//          photoHTML += '<li>';
+//          photoHTML += '<img src="' + photo.original_size.url + '"></li>';
+//        });
+//        photoHTML += '</ul>';
+//        console.log('Rendeering Tumblr');
+//        $('#tumblr').html(photoHTML);
+//        
+//      });
+
     $.ajax({
-      url: tumblrAvt,
+      url: tumblrPost,
       dataType: 'jsonp',
-      success: function (avatar) {
-        console.log();
-        $(".avatar").attr('src', avatar.response.avatar_url);
+      success: function (data) {
+        var posts = data.response.posts;
+        $.each(posts, function (i) {
+          console.log(posts[i].title, posts[i].body);
+        });
+        console.log('Loop done');
+        console.log(posts);
       }
     });
     
-    //T-Posts
-    function tumblrPosts(data) {
-      var postings = posts.response.posts,
-        text = '';
-      console.log(postings);
-      $.each(data.items, function (i, postings) {
-        var p = postings[i];
-        text += '<li><img src=' + p.photos[0].original_size.url + '><a href=' + p.post_url + '>' + p.source_title + '</a></li>';
-      });
-      $('ul').append(text);
-    }
-    $.getJSON(tumblrPost, tumblrPosts);
   });//emd tumblr
 }); // end ready
 

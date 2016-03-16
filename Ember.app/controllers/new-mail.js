8a016d362ba3465b41ab862d6a68c089c244e769
lone-DO <Lone-DO@users.noreply.github.com>
2016-03-16 14:26:57 -0400

    Attempt Failed > Scrap js...
    Last attempt for trying to install Beta Form Feature, Basically only
    thing left that needs to be HotFixed is the ability to Lock the Save
    Button till (x) amount of input has been added, atm Blank submissions
    could leave to Spam in a Real scenario. Days have been spent in attempts
    at this but to no avail...

--- Ember.app/controllers/new-mail.js
+++ Ember.app/controllers/new-mail.js
@@ -1,24 +1,24 @@
 Undertale.NewMailController = Ember.Controller.extend({
-  minText: true,
   actions: {
-    //Create mail
     save: function () {
       'use strict';
       var newMail = this.store.createRecord('mail', {
-          //attaches to {{user}}
           user: this.get('user'),
-          //attaches to {{text}}
           text: this.get('text')
         });
-      //Save Mail
       newMail.save();
       this.transitionToRoute('mailbox');
-      
-      //Disable Save Button
+    }
+  }
+});
+
+////_________   Junk/ Beta Features    _________\\
+  
+    //Disable Save Button
         //Check if User Field is Valid
           //Valid if user.val().length > 3
         //Check if Text Field is Valid
-          //Valid if text.val().length > 5
+          //Valid if text.val().length > 10
       //If Valid = True.then(Enable Save Button)
       
       //    minText: function () {
@@ -30,10 +30,64 @@ Undertale.NewMailController = Ember.Controller.extend({
         //      }
         //    }
 
-    },
-    entryNotAllowed: function () {
-      'use strict';
-      console.log('This is invalid');
-    }
-  }
-});
+////_________   JQUERY    _________\\
+////Problem: Hints are shown even when form is valid
+////Solution: Hide and show them at appropriate times
+//var $user = $('#user');
+//var $text = $('#text');
+//
+////Hide hints
+//$("#span").hide();
+//
+//function isUserValid() {
+//  'use strict';
+//  return $('#user').val.length > 3;
+//}
+//
+//function isTextValid() {
+//  'use strict';
+//  return $('#text').val.length > 10;
+//}
+//
+//function canSave() {
+//  'use strict';
+//  return isTextValid() && isUserValid();
+//}
+//
+//function userEvent() {
+//  'use strict';
+//    //Find out if password is valid  
+//  if (isUserValid()) {
+//    //Hide hint if valid
+//    $user.next().hide();
+//  } else {
+//    //else show hint
+//    $user.next().show();
+//  }
+//}
+//
+//function textEvent() {
+//  'use strict';
+//    //Find out if password is valid  
+//  if (isTextValid()) {
+//    //Hide hint if valid
+//    $text.next().hide();
+//  } else {
+//    //else show hint
+//    $text.next().show();
+//  }
+//}
+//
+//function enableSaveEvent() {
+//  'use strict';
+//  $("#save").prop("disabled", !canSave());
+//}
+//
+////When event happens on User input
+//$user.focus(userEvent).keyup(userEvent).keyup(textEvent).keyup(enableSaveEvent);
+//
+////When event happens on Text input
+//$text.focus(textEvent).keyup(textEvent).keyup(enableSaveEvent);
+//
+//enableSaveEvent();
+//console.log('Function is running');
